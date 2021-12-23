a841e325e2b49070f2b6699e0d0db212638d577a
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2021-12-23 11:53:24 +0100

    Add change from home computer

--- src/NewComponent.jsx
+++ src/NewComponent.jsx
@@ -1,6 +1,11 @@
 
 function NewComponent() {
-    return <h3>I come from development branch!!!</h3>
+    return (
+        <>
+            <h3>I come from development branch!!!</h3>
+            <p>and this is the addition from home computer</p>
+        </>
+    )
 }
 
 export default NewComponent;
